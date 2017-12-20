f3f8d860eaa204f37857d39104f08526223be6d9
Alonzo Moses <alonzo.k.moses@gmail.com>
2017-12-20 13:35:10 -0500

    updated the "db" directory and minor cleaning in "public" directory

--- db/seeds.js
+++ db/seeds.js
@@ -1,2 +1,10 @@
-const recipeManager = require('./schema')
+const Recipe = require('./schema')
 const seedData = require('./seeds.json')
+
+Recipe.remove({})
+  .then(() => {
+    return Recipe.collection.insert(seedData)
+  })
+  .then (() => {
+    process.exit()
+  })
