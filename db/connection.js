e2191b7828f77c570ab435b3d79ba91539faa806
Kathy Lambert <Kathy.Lambert@LotusDesignsAgency.com>
2017-12-20 10:57:21 -0500

    Updated db directory

---
+++ db/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/recipeManager', { useMongoClient: true })
+
+
+mongoose.Promise = Promise
+module.exports = mongoose
+// makes mongoose available elsewhere
