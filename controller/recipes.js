26e8dc31204b9bfd4a50fb24b05f9c6ce983f858
Alonzo Moses <alonzo.k.moses@gmail.com>
2017-12-21 12:40:19 -0500

    controller work in progress

--- controller/recipes.js
+++ controller/recipes.js
@@ -1,10 +1,23 @@
+// import { error } from 'util';
+
 const express = require('express')
+const db = require('../db/connection')
 const Recipe = require('../db/schema')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  Recipe.find({})
-    .then((recipes) => {
-      //incomplete will return 
-    })
+  res.send('You See Me')
 })
+// router.get('/', (req, res) => {
+//   Recipe.find({})
+//     .then((recipes) => {
+//       res.render('app-welcome', {
+//         recipes: db.recipes
+//       })
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// })
+
+module.exports = router
