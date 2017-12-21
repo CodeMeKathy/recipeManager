66a8bd5b4ae939e9b5be3dc085e79a921b6aa83f
Kathy Lambert <Kathy.Lambert@LotusDesignsAgency.com>
2017-12-21 14:19:22 -0500

    merge

--- controller/recipes.js
+++ controller/recipes.js
@@ -9,4 +9,11 @@ router.get('/', (req, res) => {
   res.send('You See Me')
 })
 
+router.post('/', (req, res) => {
+  Recipe.create(req.body.Recipe)
+    .then(Recipe => {
+      res.redirect(`/Recipe/${Recipe.title}`)
+    })
+})
+
 module.exports = router
